b629c30c71730f5c35c99e20702d779c210c4b50
numazaki345 <ryo.091979@gmail.com>
2022-08-13 08:04:21 +0900

    検索、並び替え

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,8 @@
 class Building < ApplicationRecord
   has_many :rooms, dependent: :destroy
   has_one_attached :image
+
+
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
 end
